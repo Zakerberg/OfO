6d6be14223450542c088daba0b5b084369c74b16
Zakerberg <blmichael1129@icloud.com>
2017-06-25 10:23:53 +0800

    转换成地图坐标组

--- OfO/OfO/ViewController.swift
+++ OfO/OfO/ViewController.swift
@@ -12,7 +12,7 @@ import SWRevealViewController
 class ViewController: UIViewController,MAMapViewDelegate,AMapSearchDelegate,AMapNaviWalkManagerDelegate{
     
     var mapView : MAMapView!
-    /* SearchAPI  */
+    /* SearchAPI */
     var search: AMapSearchAPI!
     var pin : MyPinAnnotation!
     var nearBySearch = true
@@ -23,21 +23,21 @@ class ViewController: UIViewController,MAMapViewDelegate,AMapSearchDelegate,AMap
     /*  屏幕中心大头针  */
     var pinView :MAPinAnnotationView!
     
-    /* 首页面板   */
+    /* 首页面板 */
     @IBOutlet weak var panelView: UIView!
     /* 首页定位按钮 */
     @IBAction func LocationBtnClick(_ sender: UIButton) {
         searchBikeNearBy()
     }
     
-    //搜索周边的小黄车
+    /* 搜索周边的小黄车 */
     func searchBikeNearBy() {
         searchCustomerLocation(mapView.userLocation.coordinate)
     }
     
     func searchCustomerLocation(_ center:CLLocationCoordinate2D)  {
         
-        /*  周边检索  */
+        /* 周边检索 */
         let request = AMapPOIAroundSearchRequest()
         request.location = AMapGeoPoint.location(withLatitude: CGFloat(center.latitude), longitude: CGFloat(center.longitude))
         request.keywords = "餐馆"
@@ -67,9 +67,6 @@ class ViewController: UIViewController,MAMapViewDelegate,AMapSearchDelegate,AMap
         walkManager = AMapNaviWalkManager()
         walkManager.delegate = self
         
-        
-        
-        
         self.navigationItem.titleView = UIImageView(image: #imageLiteral(resourceName: "ofoLogo"))
         self.navigationItem.leftBarButtonItem?.image = #imageLiteral(resourceName: "leftTopImage").withRenderingMode(.alwaysOriginal)
         self.navigationItem.rightBarButtonItem?.image = #imageLiteral(resourceName: "rightTopImage") .withRenderingMode(.alwaysOriginal)
@@ -82,16 +79,15 @@ class ViewController: UIViewController,MAMapViewDelegate,AMapSearchDelegate,AMap
             navigationItem.leftBarButtonItem?.action = #selector(SWRevealViewController.revealToggle(_:))
             view.addGestureRecognizer(revealVC.panGestureRecognizer())
         }
-        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
-//MARK: -- (大头针动画)
+    
+    //MARK: -- (大头针动画)
     func pinAnimation() {
         //坠落动画 ,Y加位移
         let endFrame = pinView.frame
@@ -103,7 +99,7 @@ class ViewController: UIViewController,MAMapViewDelegate,AMapSearchDelegate,AMap
     }
     
     
-//MARK: -- MapView Delegate
+    //MARK: -- MapView Delegate
     
     func mapView(_ mapView: MAMapView!, didAddAnnotationViews views: [Any]!)
     {
@@ -120,8 +116,26 @@ class ViewController: UIViewController,MAMapViewDelegate,AMapSearchDelegate,AMap
             }, completion: nil)
         }
     }
-
     
+    
+    /* (绘制路线) */
+    func mapView(_ mapView: MAMapView!, rendererFor overlay: MAOverlay!) -> MAOverlayRenderer! {
+        
+        if overlay is MAPolyline {
+            pin.isLockedToScreen = false
+            
+            mapView.visibleMapRect = overlay.boundingMapRect
+            
+            let renderer = MAPolylineRenderer(overlay: overlay)
+            renderer?.strokeColor = UIColor.blue
+            renderer?.lineWidth = 8.0
+            
+            return renderer
+        }
+        return nil
+    }
+    
+    /*(导航规划)*/
     func mapView(_ mapView: MAMapView!, didSelect view: MAAnnotationView!) {
         
         start = pin.coordinate
@@ -133,18 +147,18 @@ class ViewController: UIViewController,MAMapViewDelegate,AMapSearchDelegate,AMap
     }
     
     
-/*(地图初始化完成)*/
+    /* (地图初始化完成) */
     func mapInitComplete(_ mapView: MAMapView!) {
         pin = MyPinAnnotation()
         pin.coordinate = mapView.centerCoordinate
         pin.lockedScreenPoint = CGPoint(x: view.bounds.width/2, y: view.bounds.height/2)
         pin.isLockedToScreen = true
-
+        
         mapView.addAnnotation(pin)
         mapView.showAnnotations([pin], animated: true)
     }
-   
-/*(地图用户的交互)*/
+    
+    /* (地图用户的交互) */
     func mapView(_ mapView: MAMapView!, mapDidMoveByUser wasUserAction: Bool)
     {
         if wasUserAction {
@@ -153,10 +167,11 @@ class ViewController: UIViewController,MAMapViewDelegate,AMapSearchDelegate,AMap
             searchCustomerLocation(mapView.centerCoordinate)
         }
     }
-
-/*(自定义大头针)*/
+    
+    /* (自定义大头针) */
     func mapView(_ mapView: MAMapView!, viewFor annotation: MAAnnotation!) -> MAAnnotationView! {
-        /*** 用户定位的位置,不需要自定义   ***/
+        
+        /*** 用户定位的位置,不需要自定义 ***/
         if annotation is MAUserLocation {
             return nil
         }
@@ -193,7 +208,7 @@ class ViewController: UIViewController,MAMapViewDelegate,AMapSearchDelegate,AMap
         return annotationView
     }
     
-//MARK: -- Map Search Delegate(搜索) 
+    //MARK: -- Map Search Delegate(搜索)
     
     /***  搜索周边完成后的处理  ***/
     func onPOISearchDone(_ request: AMapPOISearchBaseRequest!, response: AMapPOISearchResponse!) {
@@ -225,22 +240,28 @@ class ViewController: UIViewController,MAMapViewDelegate,AMapSearchDelegate,AMap
             mapView.showAnnotations(annotations, animated: true)
             nearBySearch = !nearBySearch
         }
-        
     }
-//MARK: -- AMapNaviWalkManagerDelegate
     
+    //MARK: -- AMapNaviWalkManagerDelegate
     
     func walkManager(onCalculateRouteSuccess walkManager: AMapNaviWalkManager) {
+        print("规划路径成功:")
+    
+        mapView.removeOverlays(mapView.overlays)
         
+        var coordinates = walkManager.naviRoute!.routeCoordinates!.map {
+            
+            return CLLocationCoordinate2D(latitude: CLLocationDegrees($0.latitude), longitude: CLLocationDegrees($0.longitude))
+        }
+        
+        let polyline = MAPolyline(coordinates: &coordinates, count: UInt(coordinates.count))
+        
+        mapView.add(polyline)
     }
     
     func walkManager(_ walkManager: AMapNaviWalkManager, onCalculateRouteFailure error: Error) {
-        
+        print("规划路径失败:",error)
     }
-    
-    
-    
-    
     
 }
 
