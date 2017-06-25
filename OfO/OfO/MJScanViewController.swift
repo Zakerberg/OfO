dc8d29d127cdf4b0b8ff62ce5846122dcc964b26
Zakerberg <blmichael1129@icloud.com>
2017-06-25 13:35:02 +0800

    集成swiftScan

--- OfO/OfO/MJScanViewController.swift
+++ OfO/OfO/MJScanViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MJScanViewController: UIViewController {
+class MJScanViewController: LBXScanViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
