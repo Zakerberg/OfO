1e4616bea97c266d2ff1723b4043b2a10f5842e7
Zakerberg <blmichael1129@icloud.com>
2017-06-25 14:00:54 +0800

    闪光灯

--- OfO/OfO/MJScanViewController.swift
+++ OfO/OfO/MJScanViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class MJScanViewController: LBXScanViewController {
 
+    
+    @IBOutlet weak var flashBtn: UIButton!
+    
+    @IBOutlet weak var pannelView: UIView!
+  
+    var isFlashOn = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,8 +24,37 @@ class MJScanViewController: LBXScanViewController {
         navigationController?.navigationBar.barStyle = .blackTranslucent
         navigationController?.navigationBar.tintColor = UIColor.white
     
+        
+        var style = LBXScanViewStyle()
+        style.anmiationStyle = .NetGrid
+        style.animationImage = UIImage(named: "CodeScan.bundle/qrcode_scan_part_net")
+        
+        scanStyle = style
     }
-
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        navigationController?.navigationBar.barStyle = .default
+        navigationController?.navigationBar.tintColor = UIColor.black
+    }
+    
+    //闪光灯
+    @IBAction func flashBtnTap(_ sender: UIButton) {
+        isFlashOn = !isFlashOn
+        scanObj?.changeTorch()
+        
+        if isFlashOn {
+            flashBtn.setImage(#imageLiteral(resourceName: "btn_enableTorch"), for: .normal)
+        } else {
+            flashBtn.setImage(#imageLiteral(resourceName: "btn_torch_disable"), for: .normal)
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        view.bringSubview(toFront: pannelView)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
