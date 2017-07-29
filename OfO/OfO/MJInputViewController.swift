4da39728e0a669da269946749caa6d2f2d247296
Zakerberg <blmichael1129@icloud.com>
2017-07-29 13:40:32 +0800

    手电筒和声音按钮小改

--- OfO/OfO/MJInputViewController.swift
+++ OfO/OfO/MJInputViewController.swift
@@ -12,17 +12,34 @@ class MJInputViewController: UIViewController {
 
     @IBOutlet weak var inputTF: UITextField!
    
+    @IBOutlet weak var flashBtn: UIButton!
+    
+    @IBOutlet weak var voiceBtn: UIButton!
+    
+    var isFlashOn = false
+    var isVoiceOn = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
         inputTF.layer.borderColor = UIColor.OfO.cgColor
         inputTF.layer.borderWidth = 2
         
-        
     }
 
+    //MARK: -- BtnClick
+    
+    @IBAction func flashBtnClick(_ sender: UIButton) {
+        
+        isFlashOn = !isFlashOn
+        isFlashOn ? flashBtn.setImage(#imageLiteral(resourceName: "btn_enableTorch"), for: .normal) : flashBtn.setImage(#imageLiteral(resourceName: "btn_unenableTorch"), for: .normal)
+    }
+    
+    @IBAction func voiceBtnClick(_ sender: UIButton) {
+        
+        isVoiceOn = !isVoiceOn
+        isVoiceOn ? voiceBtn.setImage(#imageLiteral(resourceName: "voiceopen"), for: .normal) : voiceBtn.setImage(#imageLiteral(resourceName: "voiceclose"), for: .normal)
+    }
     
     
     override func didReceiveMemoryWarning() {
