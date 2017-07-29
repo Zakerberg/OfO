c2be13ad87cc0c2f3ac8e47194fbe57d6e81a3bc
Zakerberg <blmichael1129@icloud.com>
2017-07-29 15:13:42 +0800

    IBInspectable

--- OfO/OfO/ExtensionHelper.swift
+++ OfO/OfO/ExtensionHelper.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017年 Michael 柏. All rights reserved.
 //
 
+
 extension UIColor {
     
     static var OfO: UIColor {
@@ -13,3 +14,34 @@ extension UIColor {
         return UIColor(red: 247/255, green: 215/255, blue: 80/255, alpha: 1)
     }
 }
+
+
+extension UIView {
+    
+  @IBInspectable var borderWidth: CGFloat {
+        
+        get {
+            return self.layer.borderWidth
+        }
+        set {
+            self.layer.borderWidth = newValue
+        }
+    }
+    
+   @IBInspectable var borderColor: UIColor {
+        
+        get {
+            return UIColor(cgColor: self.layer.borderColor!)
+        }
+        set {
+            self.layer.borderColor = newValue.cgColor
+        }
+    }
+}
+
+
+
+
+
+
+
